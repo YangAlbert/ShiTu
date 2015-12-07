bc9a1b5b682433dc8474e28bc13d7e7ee0fb13f0
Albert Yang <ruijian.yang@gmail.com>
2015-12-07 21:47:37 +0800

    add osmdroid map module;

--- app/src/main/java/com/shitu/indoor/MapActivity.java
+++ app/src/main/java/com/shitu/indoor/MapActivity.java
@@ -1,64 +1,91 @@
 package com.shitu.indoor;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
-public class MapActivity extends Activity {
+import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.overlays.MapEventsReceiver;
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
-    RelativeLayout mNavigateLayout = null;
+import java.util.ArrayList;
+
+public class MapActivity extends Activity implements MapEventsReceiver {
+
+    public static final GeoPoint GLODON = new GeoPoint(40.044771, 116.277071);
+
+    private MapView mapView = null;
+
+    private GeoPoint start = null;
+    private GeoPoint end = null;
+
+    org.osmdroid.bonuspack.overlays.Polyline mRoadLay = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        mNavigateLayout = (RelativeLayout) findViewById(R.id.navigate_layout);
-        final TextView clickContent_tv = (TextView) findViewById(R.id.clickCotent_textView);
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setClickable(true);
+        mapView.setBuiltInZoomControls(false);
+        mapView.setMultiTouchControls(true);
+        mapView.setUseDataConnection(false);
+        mapView.setMaxZoomLevel(21);
+        // attention!!: need to rename tile folder in .zip file to "CycleMap";
+        mapView.setTileSource(TileSourceFactory.CYCLEMAP);
 
+        IMapController mapViewController = mapView.getController();
+        mapViewController.setZoom(15);
+        mapViewController.setCenter(GLODON);
 
-        // add listener to content clicker.
-        TextView.OnClickListener tvListener = new TextView.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView tv = (TextView)view;
-
-                clickContent_tv.setText("Destination: \n" + tv.getText());
-                mNavigateLayout.setVisibility(View.VISIBLE);
-            }
-        };
-
-        final TextView mapView_tv = (TextView) findViewById(R.id.mapView_textView);
-        mapView_tv.setOnClickListener(tvListener);
-
-        final TextView secondLine_tv = (TextView) findViewById(R.id.secondLine_textView);
-        secondLine_tv.setOnClickListener(tvListener);
-
-        Button navButton = (Button) findViewById(R.id.nav_button);
-        navButton.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent navActivity = new Intent(getApplicationContext(), CameraActivity.class);
-                startActivity(navActivity);
-            }
-        });
+        start = new GeoPoint(40.0443964, 116.2776609);
+        end = new GeoPoint(40.0449063, 116.2768361);
+        showRouting();
     }
 
     @Override
-    public void onBackPressed() {
-        assert mNavigateLayout != null;
-        if (mNavigateLayout.getVisibility() == View.VISIBLE) {
-            mNavigateLayout.setVisibility(View.INVISIBLE);
+    public boolean singleTapConfirmedHelper(GeoPoint p) {
+        if (start == null) {
+            start = p;
         }
         else {
-            super.onBackPressed();
+            end = p;
+            showRouting();
         }
+
+        return true;
+    }
+
+    @Override
+    public boolean longPressHelper(GeoPoint p) {
+//        return super.longPressHelper(p);
+        return true;
+    }
+
+    private void showRouting()
+    {
+        if (mRoadLay != null) {
+            mapView.getOverlays().remove(mRoadLay);
+        }
+
+        RoadManager rm = new OSRMRoadManager();
+
+        ArrayList<GeoPoint> ptArray = new ArrayList<GeoPoint>();
+        ptArray.add(start);
+        ptArray.add(end);
+        Road road = rm.getRoad(ptArray);
+
+        mRoadLay = RoadManager.buildRoadOverlay(road, this);
+        mapView.getOverlays().add(mRoadLay);
+
+        mapView.invalidate();
+
+        // invalidate start point.
+        start = null;
     }
 }
